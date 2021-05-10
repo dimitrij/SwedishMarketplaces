7ac7373b414c4fba4e741d518368e05004d95f01
unknown <arzasarvani@gmail.com>
2021-05-10 13:28:15 +0200

    product

---
+++ blocket.se/app.js
@@ -0,0 +1,35 @@
+/*fetching data by using fetch API
+
+fetch(queryURL)
+.then(function (response) {
+        return response.json();
+        // The JSON data will arrive here
+})
+
+.catch(function (error) {
+    console.log('Error during fetch: ' + error.message);
+    // If an error occured, you will catch it here
+});*/
+
+
+fetch('product.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+
+  function appendData(data) {
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+      console.log(data);
+        var div = document.createElement("div");
+        div.innerHTML = 'title: ' + data[i].description + ' ' + data[i].price;
+        mainContainer.appendChild(div);
+    }
+}
